4d6db990678796fc3c13986045800d5e096c5f4e
mahamat souradje saleh <mahamatsouradjesaleh37@gmail.com>
2025-10-24 15:18:50 +0530

    Update main.py

--- P2/P2C1/statement/main.py
+++ P2/P2C1/statement/main.py
@@ -1,6 +1,33 @@
 def main():
     # Write your code here!
     # Be careful, all your code must be indented like this comment !
+    left_number = 10
+right_number = 2
+
+symbol = '+'
+
+result = 0
+
+if not isinstance(left_number,int) or not isinstance(right_number,int):
+    print("both left_numbers and right_numbers must be integers")
+    exit()
+match symbol:
+    case '+':
+        result = left_number + right_number
+    case '-':
+        result = left_number - right_number
+    case '*':
+        result = left_number * right_number
+    case '/':
+        if  left_number / 0 :
+            print('Error: division by zero is not allowed.')
+        else :
+            result = left_number / right_number
+    case _ :
+        print('error message')
+    
+
+print(f"The result of {left_number} {symbol} {right_number} = {result}")
 
 
 # Do not modify the code below
