ee253d1abde44133df7a34f24721c5968c62fb2e
mahamat souradje saleh <mahamatsouradjesaleh37@gmail.com>
2025-10-31 15:33:50 +0530

    Update main.py

--- P3/P3C1/statement/main.py
+++ P3/P3C1/statement/main.py
@@ -1,5 +1,7 @@
 # Write your code here!
-
+from operations import addition,multiplication
+print(addition(10,5))
+print(multiplication(8,4))
 
 # Do not modify the code below
 if __name__ == "__main__":
